d16060e0c26357da26552353fc958214128b0bba
Robin paul <robinpaul288@gmail.com>
2022-01-08 16:00:00 +0530

    Project folder commit

---
+++ ShopmeWebProject/EcommerceBackedProj/src/main/java/com/robin/shopme/EcommerceBackedProjApplication.java
@@ -0,0 +1,13 @@
+package com.robin.shopme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackedProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackedProjApplication.class, args);
+	}
+
+}
---
+++ ShopmeWebProject/EcommerceFrontendProj/src/main/java/com/robin/shopme/EcommerceFrontendProjApplication.java
@@ -0,0 +1,13 @@
+package com.robin.shopme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFrontendProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceFrontendProjApplication.class, args);
+	}
+
+}
